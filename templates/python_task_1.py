9d2afb934e020025a15c2958cf475da58079a00e
Yoge-sh <yoshdis20@gmail.com>
2023-12-09 23:34:21 +0530

    Python task 1 completed

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import warnings
+warnings.filterwarnings("ignore")
 
 def generate_car_matrix(df)->pd.DataFrame:
     """
@@ -13,6 +14,15 @@ def generate_car_matrix(df)->pd.DataFrame:
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
     # Write your logic here
+    pivot_table = df.pivot(index='id_1', columns='id_2', values='car').fillna(0)
+
+    # Set diagonal values to 0
+    for i in pivot_table.index:
+        pivot_table.loc[i, i] = 0
+    # round all values to 1 decimal point
+    pivot_table = pivot_table.round(1)
+    df = pd.DataFrame(pivot_table)
+    df = df*2
 
     return df
 
@@ -28,8 +38,16 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
-
-    return dict()
+    def car_type(x):
+        if x<=15:
+            return 'low'
+        elif x>25:
+            return 'high'
+        else:
+            return 'medium'
+    df['car_type'] = df['car'].apply(car_type)
+    
+    return dict(df['car_type'].value_counts())
 
 
 def get_bus_indexes(df)->list:
@@ -43,8 +61,7 @@ def get_bus_indexes(df)->list:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
     # Write your logic here
-
-    return list()
+    return sorted(list(df.index[df['bus']>2*df['bus'].mean()]))
 
 
 def filter_routes(df)->list:
@@ -58,8 +75,10 @@ def filter_routes(df)->list:
         list: List of route names with average 'truck' values greater than 7.
     """
     # Write your logic here
-
-    return list()
+    route_avg = df.groupby('route')['truck'].mean()
+    # Filter routes where the average of 'truck' is greater than 7
+    routes = route_avg[route_avg > 7].index
+    return list(sorted(routes))
 
 
 def multiply_matrix(matrix)->pd.DataFrame:
@@ -73,7 +92,7 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
-
+    matrix = matrix.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25).round(1)
     return matrix
 
 
@@ -88,5 +107,47 @@ def time_check(df)->pd.Series:
         pd.Series: return a boolean series
     """
     # Write your logic here
+    # Create a boolean column to modify further
+    df['Correct_timestamps'] = df['id'].map(lambda x: (x/3).is_integer())
+    grouped_df = df.groupby(['id','id_2']).filter(lambda x: True)
+    day_mapping = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
 
-    return pd.Series()
+    i = 0
+    while i != len(grouped_df):
+        if (grouped_df['startTime'].iloc[i] == '00:00:00') & (grouped_df['endTime'].iloc[i] == '23:59:59') & (day_mapping[grouped_df['startDay'].iloc[i]]- day_mapping[grouped_df['endDay'].iloc[i]] <=0):
+            grouped_df['Correct_timestamps'].iloc[i]=True
+        else:
+            grouped_df['Correct_timestamps'].iloc[i]=False
+        i+=1    
+    id_series = grouped_df.iloc[:,0]
+    id_2_series = grouped_df.iloc[:,2]
+    is_correct = grouped_df.iloc[:,-1]
+    # new boolean Series with multi-index
+    combined_series = pd.Series(index=pd.MultiIndex.from_arrays([id_series, id_2_series,is_correct ], names=['id', 'id_2','is_correct_timestamp']))
+    combined_series
+
+    return combined_series
+
+if __name__=='__main__':
+    # print('Yoyouiou')
+    df = pd.read_csv('datasets\dataset-1.csv')
+
+    #q1
+    # print(generate_car_matrix(df))
+
+    #q2
+    # print(get_type_count(df))
+
+    #q3
+    # print(get_bus_indexes(df))
+
+    #q4
+    # print(filter_routes(df))
+
+    # #q5
+    # matrix = generate_car_matrix(df)
+    # print(multiply_matrix(matrix))
+
+    #q6
+    # df2 = pd.read_csv('datasets\dataset-2.csv')
+    # print(time_check(df2))
